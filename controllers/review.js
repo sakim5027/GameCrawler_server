eb93f2ced87e88078bed503d0604bb074c807433
mkkang03 <devMinkyoung@gmail.com>
2021-04-19 20:44:07 +0900

    #12 modifyReview

--- controllers/review.js
+++ controllers/review.js
@@ -3,7 +3,7 @@ module.exports = {
     addReview: async (req, res) => {
         const { game_id, rate, story, graphic, hardness, music, ux, contents } = req.body;
 
-        //db에 user정보 저장
+        //db에 review정보 저장
         const result = await review.create({
             game_id, rate, story, graphic, hardness, music, ux, contents,
             user_id:req.session.user_id
@@ -12,6 +12,21 @@ module.exports = {
             res.status(500).send("post review error");
         } else {
             res.send("success post review");
+        }
+    },
+    modifyReview: async (req, res) => {
+        const { review_id, rate, story, graphic, hardness, music, ux, contents } = req.body;
+
+        //db의 review정보 수정
+        const result = await review.update({ rate, story, graphic, hardness, music, ux, contents },
+            {
+                where: { id:review_id }
+            });
+
+        if (!result) {
+            res.status(500).send("put review error");
+        } else {
+            res.send("success put review");
         }
     }
 };
